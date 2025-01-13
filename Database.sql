d630deb1fffeae38d9582e132d24231bdb74a18a
Nada Zirari <zirarinada20@gmail.com>
2025-01-13 15:48:30 +0100

    added the databases tables for courses and users

---
+++ Database.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE youdemy;
+
+USE youdemy;
+
+-- Table pour les utilisateurs
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('student', 'teacher') NOT NULL
+   
+);
+
+-- Table pour les cours
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    teacher_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    content TEXT NOT NULL,
+    category VARCHAR(100),
+    tags VARCHAR(255),
+    -- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (teacher_id) REFERENCES users(id) ON DELETE CASCADE
+);
