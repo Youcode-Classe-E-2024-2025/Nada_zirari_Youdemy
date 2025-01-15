aa0676a441f17aa21387cbac2dd65d6a16e0ab4e
Nada Zirari <zirarinada20@gmail.com>
2025-01-15 16:47:42 +0100

    add course file

--- add_cours.php
+++ add_cours.php
@@ -1,23 +1,26 @@
 <?php
 session_start();
-require_once '../config.php';
-require_once './classes/Course.php';
+require_once 'config.php';
+require_once 'classes/Course.php';
 require_once 'classes/Auth.php';
 
-// Vérification de rôle
 Auth::checkRole('teacher');
 
-// Ajouter un cours
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $title = htmlspecialchars($_POST['title']);
+    $description = htmlspecialchars($_POST['description']);
+    $content = htmlspecialchars($_POST['content']);
+    $category = htmlspecialchars($_POST['category']);
+    $teacherId = $_SESSION['user_id'];
+
     $course = new Course();
-    $course->addCourse(
-        $_SESSION['user_id'],
-        $_POST['title'],
-        $_POST['description'],
-        $_POST['content'],
-        $_POST['category']
-    );
-    header('Location: teacher_dashboard.php?success=true');
+    $success = $course->addCourse($teacherId, $title, $description, $content, $category);
+
+    if ($success) {
+        header('Location: teacher_dashboard.php?success=1');
+    } else {
+        header('Location: teacher_dashboard.php?error=1');
+    }
     exit();
 }
 ?>
